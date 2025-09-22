2866ab0ae5652cb9b8efb62a78a83f5a8d8bc9ae
valentina0612 <vbecag@correo.usbcali.edu.co>
2025-09-22 12:04:00 -0500

    Dockerfile

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ app.mount("/", controlador_app)
 # =========================
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Render asigna el puerto aquí
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
