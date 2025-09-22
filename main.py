6f5146094aa4fab15f3a3c4c9ab652b17b055a11
Valentina Beca <126031586+valentina0612@users.noreply.github.com>
2025-09-21 21:26:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
 # Importar el controlador principal
 from ControladorApi import app as controlador_app
 
@@ -26,4 +26,6 @@ app.mount("/", controlador_app)
 # Punto de entrada
 # =========================
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Render asigna el puerto aquí
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
